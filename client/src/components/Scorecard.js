b60650a456dc284a337dab0340cf5c72e8ce6da2
Chris Pajtas <chrispaj2013@gmail.com>
2021-12-28 21:24:36 -0500

    committing

--- client/src/components/Scorecard.js
+++ client/src/components/Scorecard.js
@@ -22,20 +22,27 @@ const Scorecard = (props) => {
     var playerInputs = [];
     var roundInputs = [];
 
-    for (let i = 1; i <= props.settings[0]; i++) {
-        for (let j = 1; j <= props.settings[1]; j++) {
+    var roundLabels = [];
+
+    var numPlayers = props.settings[0];
+    var numRounds = props.settings[1];
+
+    for (let i = 1; i <= numPlayers; i++) {
+        for (let j = 1; j <= numRounds; j++) {
+            if (i == 1) {
+                playerInputs.push(<Col>Round {j}</Col>)
+            }
             roundInputs.push(
-                <Input name={`player${i}-round${j}`} />
+                <Col><Input class="form-control form-control-sm m-3" name={`player${i}-round${j}`} /></Col>
             )
         }
-        playerInputs.push(<Col> {roundInputs} </Col>);
+        playerInputs.push(<Col>{roundInputs} </Col>);
         roundInputs = [];
     }
 
-    function outputPlayerInputs(inputs) {
-        return (
-            <Col> {inputs} </Col>
-            )
+    // Outputs the labels for the round number
+    for (let i = 1; i <= numRounds; i++) {
+        roundLabels.push(<Row class="w-8 bg-warning"> <p class="h-25">Round {i}</p></Row>)
     }
 
     function outputInputs() {
@@ -47,12 +54,22 @@ const Scorecard = (props) => {
 
     return (
         <div>
-            <Container>
-
+            { //class="container-fluid d-inline"
+            }
+            <Container class="d-table">
+                {
+                    // to do
+                    // add name box, add labels for rounds
+                }
+               
                 <Row>
+                 
+               
                     {playerInputs}
                 </Row>
-                 Players: {props.settings[0]} Rounds: {props.settings[1]}
+                   
+             
+                 Players: {numPlayers} Rounds: {numRounds}
             </Container>            
         </div>
     );
